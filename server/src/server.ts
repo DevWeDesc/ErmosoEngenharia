6f296fba0e672fa4854cd4d2b4159e4a64174d57
Glaucoed <gesverzuthe@gmail.com>
2023-08-15 18:00:58 -0300

    feat: implementando docker

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import {fastify} from 'fastify'
+import {FastifyReply, FastifyRequest, fastify} from 'fastify'
 import cors from '@fastify/cors'
 import { usersRoutes } from './routes/users.routes'
 import { reportRoutes } from './routes/reports.routes'
@@ -8,8 +8,11 @@ app.register(cors, {origin: true})
 
 app.register(usersRoutes)
 app.register(reportRoutes)
+app.get('/', async (_req: FastifyRequest, reply: FastifyReply) => {
+  reply.send({ api: 'ok' })
+})
 
-app.listen({ port: 3333 }, (err, address) => {
+app.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
